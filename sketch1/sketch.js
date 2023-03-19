99aba383f1548d490db158ebbec92fc4fae8b7fb
Maya Wills <mayawills99@gmail.com>
2023-03-19 16:26:01 +1000

    Started building inital sketch

--- sketch1/sketch.js
+++ sketch1/sketch.js
@@ -1,7 +1,52 @@
+// Brightness to fill arcs with
+let b = 0;
+
+// Setting angle and angle speed
+let angle = 0;
+let angleSpeed = 0.05;
+
+// Arrays to hold the drawing data
+let xValues = [];
+let yValues = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB)
+  cursor(CROSS)
+  rectMode(CENTER)
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  // Colour will change over time
+  b = b + 1; 
+  if (b >= 100 ){
+    b = 0;
+  }
+
+  // Setting the arc to just be a line and the weight 
+  noFill()
+  strokeWeight(25)
+  stroke(120, 50, b)
+
+  if (mouseIsPressed){
+    // Store the x and y postion in the arrays
+    xValues.push(mouseX);
+    yValues.push(mouseY);
+  }
+
+  // Draw over the line data and draw to screen
+  for (let i = 0; i < xValues.length; i++){
+    let x = xValues[i];
+    let y = yValues[i];
+    
+    push()
+    translate (mouseX, mouseY);
+    rotate(radians(angle));
+    circle(x, y, 20)
+    angle += angleSpeed;
+    pop()
+  }
+
 }
